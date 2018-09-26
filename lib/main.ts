0a0c0feccbd36beb6bf688e35a373cc34be0603f
CJ Harries <cj@wizardsoftheweb.pro>
2018-09-26 14:48:24 -0500

    Switch from function to arrow not. for this inheritance and style

--- lib/main.ts
+++ lib/main.ts
@@ -2,38 +2,26 @@ import * as http from "http";
 import * as createHandler from "github-webhook-handler";
 import { handler } from "github-webhook-handler";
 
+// function
+
 const eventHandler: handler = (createHandler as any)({
     path: "/webhook",
     secret: "myhashsecret",
 });
 
-eventHandler.on("pull_request", function(event: any) {
+eventHandler.on("pull_request", (event: any) => {
     console.log("Received a PR event for %s to %s",
         event.payload.repository.name,
         event.payload.ref)
+    console.log(event.payload.pull_request.body);
 });
 
-eventHandler.on("push", function(event: any) {
-    console.log("Received a push event for %s to %s",
-        event.payload.repository.name,
-        event.payload.ref)
-});
-
-eventHandler.on("error", function(err: any) {
+eventHandler.on("error", (err: any) => {
     console.error("Error:", err.message)
 });
 
-eventHandler.on("issues", function(event: any) {
-    console.log("Received an issue event for %s action=%s: #%d %s",
-        event.payload.repository.name,
-        event.payload.action,
-
-        event.payload.issue.number,
-        event.payload.issue.title)
-});
-
-http.createServer(function(req: any, res: any) {
-    eventHandler(req, res, function(err: any) {
+http.createServer((req: any, res: any) => {
+    eventHandler(req, res, (err: any) => {
         console.log(err)
         res.statusCode = 404;
         console.log("entered");
